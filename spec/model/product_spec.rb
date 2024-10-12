abc68d8d0309304d79e1ac17eacb3a38090c1f4b
Ann <a.m.hnatyshyn@gmail.com>
2024-10-12 18:26:49 -0300

    add cypress test for homepage

--- spec/model/product_spec.rb
+++ spec/model/product_spec.rb
@@ -13,6 +13,17 @@ RSpec.describe Product, type: :model do
         quantity: 8,
         category: @category
       )
+
+      it("There is products on the page", () => {
+        cy.visit("/");
+        cy.get(".products article").should("be.visible");
+      });
+
+      it("There is 2 products on the page", () => {
+       cy.visit("/");
+       cy.get(".products article").should("have.length", 2);
+      });
+
       expect(@product).to be_valid
     end
   end
